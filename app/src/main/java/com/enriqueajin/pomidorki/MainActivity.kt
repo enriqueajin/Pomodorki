f6f64b636ba616f193a84831bb5210395f489f76
Enrique Ajin <enriquesaon@gmail.com>
2024-09-24 16:18:01 +0200

    feat(project-structure): Create packages data, di, domain, presentation, and utils to define the MVVM project structure.

--- app/src/main/java/com/enriqueajin/pomidorki/MainActivity.kt
+++ app/src/main/java/com/enriqueajin/pomidorki/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.enriqueajin.pomidorki.ui.theme.PomidorkiTheme
+import com.enriqueajin.pomidorki.presentation.ui.theme.PomidorkiTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/enriqueajin/pomidorki/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/enriqueajin/pomidorki/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.enriqueajin.pomidorki.ui.theme
+package com.enriqueajin.pomidorki.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
