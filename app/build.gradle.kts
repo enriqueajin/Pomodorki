b35f5e6d37d937f64123206a114ac6965784f1e8
Enrique Ajin <enriquesaon@gmail.com>
2024-09-25 10:54:03 +0200

    Add Firebase Auth and Firestore dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.crashlytics)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
