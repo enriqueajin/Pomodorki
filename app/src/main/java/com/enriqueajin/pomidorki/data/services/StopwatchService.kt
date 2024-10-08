117de7191d322462751aed6b197b6f599a4de4d9
Enrique Ajin <enriquesaon@gmail.com>
2024-10-08 21:55:45 +0200

    Create PendingIntent of the MainActivity in order to setContentIntent in notification (Open activity when tapping notification). Add 'singleTop' launchMode for activity within AndroidManifest.xml.

--- app/src/main/java/com/enriqueajin/pomidorki/data/services/StopwatchService.kt
+++ app/src/main/java/com/enriqueajin/pomidorki/data/services/StopwatchService.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import com.enriqueajin.pomidorki.R
+import com.enriqueajin.pomidorki.presentation.MainActivity
 
 class StopwatchService : Service() {
 
@@ -15,16 +16,24 @@ class StopwatchService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        // Intent to launch the MainActivity
+        val activityIntent = Intent(this, MainActivity::class.java).apply {
+            action = Intent.ACTION_MAIN
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+        val flag = PendingIntent.FLAG_IMMUTABLE
+        val activityPendIntent = PendingIntent.getActivity(this, 0, activityIntent, flag)
+
         when (intent?.action) {
-            Action.START.toString() -> start(intent)
+            Action.START.toString() -> start(intent, activityPendIntent)
             Action.STOP.toString() -> stopSelf()
-            Action.PAUSE.toString() -> onPause(intent)
+            Action.PAUSE.toString() -> onPause(intent, activityPendIntent)
         }
         return super.onStartCommand(intent, flags, startId)
     }
 
-
-    private fun start(intent: Intent) {
+    private fun start(intent: Intent, activityIntent: PendingIntent) {
         val flag =
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
                 PendingIntent.FLAG_IMMUTABLE
@@ -42,12 +51,13 @@ class StopwatchService : Service() {
             .setSmallIcon(R.drawable.filled_timer)
             .setContentTitle("Stopwatch")
             .setContentText("Elapsed time: 25:00")
+            .setContentIntent(activityIntent)
             .addAction(0, "PAUSE", pendingIntent)
             .build()
         startForeground(1, notification)
     }
 
-    private fun onPause(intent: Intent) {
+    private fun onPause(intent: Intent, activityIntent: PendingIntent) {
         val flag =
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
                 PendingIntent.FLAG_IMMUTABLE
@@ -77,6 +87,7 @@ class StopwatchService : Service() {
             .setSmallIcon(R.drawable.filled_timer)
             .setContentTitle("Stopwatch")
             .setContentText("Elapsed time: 25:00")
+            .setContentIntent(activityIntent)
             .addAction(0, "RESUME", resumePendingIntent)
             .addAction(0, "RESET", resetPendingIntent)
             .addAction(0, "CANCEL", cancelPendingIntent)
